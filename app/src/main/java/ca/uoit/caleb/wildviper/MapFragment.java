005b9d5b6e3a42a439836b60ffe7dda150aff328
Caleb Phillips <calebrphillips@gmail.com>
2016-11-15 13:23:35 -0500

    Handle map permissions and enable move to location button in map

--- app/src/main/java/ca/uoit/caleb/wildviper/MapFragment.java
+++ app/src/main/java/ca/uoit/caleb/wildviper/MapFragment.java
@@ -1,10 +1,16 @@
 package ca.uoit.caleb.wildviper;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.Fragment;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v13.app.FragmentCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,17 +21,30 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
 
+    //TODO Delete this var and logs
+    private static final String PERMS = "PERMISSIONS";
+
+    private static final int RC_PERMISSION_REQUEST = 1231;
+
     private GoogleMap mMap;
     private MapView mMapView;
+    private Activity mActivity;
+    private LatLng mDefaultLocation;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_map, container, false);
 
+        mActivity = getActivity();
+
+        //Set default location to Sydney
+        mDefaultLocation= new LatLng(-34, 151);
+
         // Gets the MapView from the XML layout and creates it
         mMapView = (MapView) v.findViewById(R.id.map);
         mMapView.onCreate(savedInstanceState);
+
         mMapView.getMapAsync(this);
         return v;
     }
@@ -61,9 +80,53 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        turnOnMyLocationButton();
+
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng userLocation = getUserLocation();
+        String markerString = "You are here!";
+        mMap.addMarker(new MarkerOptions().position(userLocation).title(markerString));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
+    }
+
+    private void moveToLocation(LatLng location, String marker) {
+        mMap.addMarker(new MarkerOptions().position(location).title(marker));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+    }
+
+    private void turnOnMyLocationButton() {
+        if (mActivity.checkCallingOrSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+            moveToLocation(mDefaultLocation, "You are here!");
+        } else {
+            requestLocationPermissions();
+        }
+    }
+
+    private void requestLocationPermissions() {
+        FragmentCompat.requestPermissions(
+                this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                RC_PERMISSION_REQUEST);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case RC_PERMISSION_REQUEST: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    turnOnMyLocationButton();
+                } else {
+                    Toast.makeText(mActivity, "Moving to default location.", Toast.LENGTH_LONG).show();
+                    moveToLocation(mDefaultLocation, "Default Location");
+                }
+                return;
+            }
+        }
+    }
+
+
+    private LatLng getUserLocation() {
+        return new LatLng(-34, 151);
     }
 }
