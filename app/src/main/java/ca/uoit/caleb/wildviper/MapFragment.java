4c31bb0f2ce9a7ee5390636078a5056a31c209a7
Caleb Phillips <calebrphillips@gmail.com>
2016-11-15 16:42:09 -0500

    Cleanup/Refactor Map Fragment File

--- app/src/main/java/ca/uoit/caleb/wildviper/MapFragment.java
+++ app/src/main/java/ca/uoit/caleb/wildviper/MapFragment.java
@@ -6,7 +6,6 @@ import android.app.Fragment;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v13.app.FragmentCompat;
 import android.view.LayoutInflater;
@@ -14,7 +13,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -24,7 +22,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks {
 
     private static final int RC_PERMISSION_REQUEST = 1231;
 
@@ -33,7 +31,9 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleA
     private MapView mMapView;
     private Activity mActivity;
     private LatLng mDefaultLocation;
+    private String mDefaultMarkerMessage = "Default Location";
     private LatLng mUserLocation;
+    private String mUserMarkerMessage = "You are here!";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -51,7 +51,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleA
         if (mGoogleApiClient == null) {
             mGoogleApiClient = new GoogleApiClient.Builder(mActivity)
                     .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
                     .addApi(LocationServices.API)
                     .build();
         }
@@ -68,6 +67,87 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleA
         mMapView.onCreate(savedInstanceState);
         mMapView.getMapAsync(this);
         return v;
+    }
+
+    private void setCurrentLocation() {
+        if (mActivity.checkCallingOrSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            if (location != null) {
+                mUserLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                if (mMap != null) {
+                    moveToLocation(mUserLocation, mUserMarkerMessage);
+                }
+            }
+        } else {
+            requestLocationPermissions();
+        }
+    }
+
+    private void moveToLocation(LatLng location, String marker) {
+        mMap.addMarker(new MarkerOptions().position(location).title(marker));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+    }
+
+
+    private void requestLocationPermissions() {
+        FragmentCompat.requestPermissions(
+                this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                RC_PERMISSION_REQUEST);
+    }
+
+    /**
+     * Callback when user accepts or rejects Location Permission
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case RC_PERMISSION_REQUEST: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    setCurrentLocation();
+                } else {
+                    Toast.makeText(mActivity, "Moving to default location.", Toast.LENGTH_LONG).show();
+                    moveToLocation(mDefaultLocation, mDefaultMarkerMessage);
+                }
+                return;
+            }
+        }
+    }
+
+
+    /**
+     * Map has been loaded
+     * @param googleMap
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        if (mUserLocation == null) {
+            moveToLocation(mDefaultLocation, mDefaultMarkerMessage);
+        } else {
+            moveToLocation(mUserLocation, mUserMarkerMessage);
+        }
+    }
+
+    /**
+     * Google Api Connected Success
+     * @param bundle
+     */
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        setCurrentLocation();
+    }
+
+    /**
+     * Google Api connection suspended
+     * @param i
+     */
+    @Override
+    public void onConnectionSuspended(int i) {
+
     }
 
     @Override
@@ -99,92 +179,5 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleA
     public void onLowMemory() {
         super.onLowMemory();
         mMapView.onLowMemory();
-    }
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        if (mUserLocation == null) {
-            moveToLocation(mDefaultLocation, "Sydney");
-        } else {
-            moveToLocation(mUserLocation, "You are here!");
-        }
-    }
-
-    private void setCurrentLocation() {
-        if (mActivity.checkCallingOrSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-            mUserLocation = new LatLng(location.getLatitude(), location.getLongitude());
-            if (mMap != null) {
-                moveToLocation(mUserLocation, "You are here!");
-            }
-        } else {
-            requestLocationPermissions();
-        }
-    }
-
-    private void moveToLocation(LatLng location, String marker) {
-        mMap.addMarker(new MarkerOptions().position(location).title(marker));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
-    }
-
-
-    private void requestLocationPermissions() {
-        FragmentCompat.requestPermissions(
-                this,
-                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                RC_PERMISSION_REQUEST);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case RC_PERMISSION_REQUEST: {
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    setCurrentLocation();
-                } else {
-                    Toast.makeText(mActivity, "Moving to default location.", Toast.LENGTH_LONG).show();
-                    moveToLocation(mDefaultLocation, "Default Location");
-                }
-                return;
-            }
-        }
-    }
-
-
-    /**
-     * Google Api Connected Success
-     * @param bundle
-     */
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        setCurrentLocation();
-    }
-
-    /**
-     * Google Api connection suspended
-     * @param i
-     */
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    /**
-     * Google Api connection failed
-     * @param connectionResult
-     */
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
     }
 }
